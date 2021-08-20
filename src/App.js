9d8f8f7938f9eecda49ca837b3b50c1940dd58ee
Michael <mkenton2020@icloud.com>
2021-08-20 12:07:43 -0500

    working with deliverables

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import BillCollection from './components/BillCollection';
 import BillsCast from './components/BillsCast';
 
+const API = 'http://localhost:8002/bills'
+
 export default function App() {
   
 const [allBills, setAllBills] = useState([])
@@ -9,17 +11,37 @@ const [allBills, setAllBills] = useState([])
 useEffect( () => {
   fetch(API)
   .then((res) => res.json())
-  .then((json) => setAllBills(json))
-
-
+  .then((data) => setAllBills(data))
 }, [] );
 
+
+function setEnlisted(enlistedBill) {
+  // console.log(`set bill ${enlistedBill} to enlisted`)
+  setAllBills(allBills.map((bill) =>
+    bill.id === enlistedBill ? { ...bill, enlisted: true } : bill
+  ))
+}
+
+function setDelisted(DelistedBill) {
+  // console.log(`set bill ${DelistedBill} to enlisted`)
+  setAllBills(allBills.map((bill) =>
+    bill.id === DelistedBill ? { ...bill, enlisted: false } : bill
+  ))
+}
+
+const myBills = allBills.filter((bill) => bill.enlisted);
+
+function fireBill(billToFire) {
+  console.log(`set bill ${billToFire} to Fired`);
+
+  setAllBills(allBills.filter(bill => bill.id !== billToFire));
+}
 
 
   return (
     <div>
-      <BillsCast />
-      <BillCollection />
+      <BillsCast myBills={myBills} onClickBill={setDelisted} onFireBill={fireBill}/>
+      <BillCollection allBills={allBills} onClickBill={setEnlisted} onFireBill={fireBill}/>
     </div>
   );
 }
--- src/components/BillsCast.js
+++ src/components/BillsCast.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import BillCard from './BillCard'
 
-export default function BillsCast() {
+export default function BillsCast({myBills, onClickBill, onFireBill}) {
   // Your code here
 
   return (
     <div className="ui segment inverted blue bill-cast">
       <div className="ui five column grid">
         <div className="row bill-cast-row">
-        {/*...and here..*/}
+        {myBills.map((bill) => (
+        <BillCard 
+        key={bill.id} 
+        bill={bill}
+        onClickBill={onClickBill}
+        onFireBill={onFireBill} 
+         /> ))}
         </div>
       </div>
     </div>
